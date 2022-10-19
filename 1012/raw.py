1b6b3a68c682962a6ab3b634eb1fbcae659a1013
Paul <95068443+paulchen2713@users.noreply.github.com>
2022-10-19 12:39:15 +0800

    Add files via upload

---
+++ 1012/raw.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 05 10:28:24 2022
+
+@patch: 2022.10.05
+@author: Paul
+@file: raw.py
+@dependencies:
+    envs        test
+    python      3.6.13
+    ipython     7.16.1
+    ipykernel   5.3.4
+    numpy       1.19.5 (numpy-base 1.19.2)
+    scipy       1.5.4
+    torch       1.10.2
+    torchvision 0.11.3
+    matplotlib  3.3.4
+"""
+
+from operator import ge
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision.transforms as T # for resizing the images
+from PIL import Image              # for loading and saving the images
+
+# set the dataset path
+DATASET = 'D:/Datasets/CARRADA/Carrada/'
+CURR_PATH = 'D:/BeginnerPythonProjects/read_carrada/'
+
+# set the file index
+seq_name = '2020-02-28-13-09-58'
+instances = ['001114', '001115']
+frame_name = '000117'
+
+# set the file path
+seq_path = DATASET + seq_name + '/'
+rd_path = seq_path + 'range_doppler_numpy/' + frame_name + '.npy'
+ra_path = seq_path + 'range_angle_numpy/' + frame_name + '.npy'
+img_path = seq_path + 'camera_images/' + frame_name + '.jpg'
+
+# load the RDM, RAM
+ra_matrix = np.load(ra_path)
+print(f"ra_matrix.shape = {ra_matrix.shape}")
+rd_matrix = np.load(rd_path)
+print(f"rd_matrix.shape = {rd_matrix.shape}")
+
+# print("rd_matrix: ")
+# print(rd_matrix)
+# print("ra_matrix: ")
+# print(ra_matrix)
+
+# a_file = open("test.txt", "a")
+# for row in rd_matrix:
+#     np.savetxt(a_file, row)
+# a_file.close()
+
+def get_shape(matrix):
+    x, y = matrix.shape
+    print(f"x, y = {x}, {y}")
+
+get_shape(rd_matrix)
+np.savetxt(CURR_PATH + 'logs/000117_256_64.txt', rd_matrix, delimiter=' ')
+
+
+
+# plot the matric with its original shape, rd_matrix.shape = (256, 64)
+# plt.imshow(rd_matrix) 
+plt.matshow(rd_matrix, interpolation="nearest")
+plt.plasma()
+# plt.axis('off')
+plt.savefig(CURR_PATH + 'figs/rd_matrix.png', bbox_inches='tight', pad_inches=0)
+
+rrdm = np.rot90(rd_matrix, k=2) # rotate 180 degrees to fit cartesian coordinates
+plt.matshow(rrdm, interpolation="nearest")
+plt.plasma()
+# plt.axis('off')
+plt.savefig(CURR_PATH + 'figs/rrdm.png', bbox_inches='tight', pad_inches= 0)
+
+
+# plt.xlim(-13.5, 13.5)
+# plt.ylim(0, 50)
+plt.savefig(CURR_PATH + 'figs/000117_256_64.png')
+
+# get_shape(ra_matrix)
+# np.savetxt('D:/BeginnerPythonProjects/read_carrada/logs/000117_256_256.txt', ra_matrix, delimiter=' ')
+# plt.imshow(ra_matrix, extent=[0, 1, 0, 1])
+# plt.savefig("D:/BeginnerPythonProjects/read_carrada/figs/000117_256_256.png")
+
+
+plt.show()
+
---
+++ 1012/samples2.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 05 10:28:01 2022
+
+@patch: 2022.10.05
+@author: Paul
+@file: sample.py
+@dependencies:
+    envs        test
+    python      3.6.13
+    ipython     7.16.1
+    ipykernel   5.3.4
+    numpy       1.19.5 (numpy-base 1.19.2)
+    scipy       1.5.4
+    torch       1.10.2
+    torchvision 0.11.3
+    matplotlib  3.3.4
+"""
+
+import json
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# from carrada_dataset.utils.configurable import Configurable
+from visualize_signal import SignalVisualizer
+# from carrada_dataset.utils.transform_annotations import AnnotationTransformer
+from generate_annotations import AnnotationGenerator
+# from carrada_dataset.utils.transform_data import DataTransformer
+
+
+
+def main():
+    # Sample to visualize
+    seq_name = '2020-02-28-13-09-58'
+    instances = ['001114', '001115']
+    frame_name = '000117'
+
+
+    # Define paths
+    DATASET = 'D:/Datasets/CARRADA/Carrada/'
+
+    seq_path = DATASET + seq_name + '/'
+    print(f"seq_path = {seq_path}") # seq_path = D:/Datasets/CARRADA/Carrada/2020-02-28-13-09-58/
+
+    rd_path = seq_path + 'range_doppler_numpy/' + frame_name + '.npy'
+    print(f"rd_path = {rd_path}") # rd_path = D:/Datasets/CARRADA/Carrada/2020-02-28-13-09-58/range_doppler_numpy/000117.npy
+
+    ra_path = seq_path + 'range_angle_numpy/' + frame_name + '.npy'
+    print(f"ra_path = {ra_path}") # ra_path = D:/Datasets/CARRADA/Carrada/2020-02-28-13-09-58/range_angle_numpy/000117.npy
+
+    img_path = seq_path + 'camera_images/' + frame_name + '.jpg'
+    print(f"img_path = {img_path}") # img_path = D:/Datasets/CARRADA/Carrada/2020-02-28-13-09-58/camera_images/000117.jpg
+
+    """
+    # Path to the RAD tensor (soon available)
+    rad_path = os.path.join(seq_path, 'RAD_numpy', frame_name + '.npy')
+    """
+
+    # annotations_path = os.path.join(DATASET, 'annotations_frame_oriented.json')
+    annotations_path = DATASET + 'annotations_frame_oriented.json'
+    print(f"annotations_path = {annotations_path}")
+
+    # Load data
+
+    # Annotations
+    with open(annotations_path, 'r') as fp:
+        annotations = json.load(fp)
+    annotations = annotations[seq_name][frame_name]  # Keep annotations of interest
+    # print(f"annotations = {annotations}")
+
+
+    # Range-angle and range-Doppler matrices
+    """
+    # the RA and RD matrices can be computed directly from the RAD tensor
+    rad_matrix = np.load(rad_path)
+    data_transformer = DataTransformer(rad_matrix)
+    ra_matrix = data_transformer.to_ra()
+    rd_matrix = data_transformer.to_rd()
+    """
+    ra_matrix = np.load(ra_path)
+    print(f"ra_matrix.shape = {ra_matrix.shape}")
+    rd_matrix = np.load(rd_path)
+    print(f"rd_matrix.shape = {rd_matrix.shape}")
+
+
+    # Camera image of the scene
+    # print('Camera image of the scene {}, frame {}'.format(seq_name, frame_name))
+    print(f"Camera image of the scene {seq_name}, frame {frame_name}")
+    # Camera image of the scene 2020-02-28-13-09-58, frame 000117
+    img = Image.open(img_path) # 
+    # print(f"img: {img}")
+    # plt.imshow(img)
+    # plt.show()
+
+    # display(img)
+
+
+    # Range-Doppler visualization
+    signal_visualizer = SignalVisualizer(rd_matrix)
+    print('Raw Range-Doppler representation:')
+    signal_visualizer.save_scale(path='.', signal_type='range_doppler', color_scale=False, rotation=True, save_img=False, plot_img=True)
+
+    for annotation_type in ['sparse', 'dense', 'box']:
+        for i, instance in enumerate(instances):
+            points = annotations[instance]['range_doppler'][annotation_type]
+            annot_generator = AnnotationGenerator(rd_matrix.shape, points)
+            if annotation_type is 'sparse':
+                annots = annot_generator.get_points()
+            elif annotation_type is 'dense':
+                annots = annot_generator.get_mask()
+            else:
+                annots = annot_generator.get_box()
+            signal_visualizer.add_annotation(i, annots, annotation_type)
+        print(f'Range-Doppler with {annotation_type} annotations:')
+        signal_visualizer.save_multiple_annotations(path=f'/root/workspace/temp/rd_{annotation_type}.png', signal_type='range_doppler', color_scale=False, rotation=True, save_img=False, plot_img=True)
+        signal_visualizer.reset_annotation()
+
+
+    # Range-Angle visualization
+    signal_visualizer = SignalVisualizer(ra_matrix)
+    print('Raw Range-Angle representation:')
+    signal_visualizer.save_scale(path='.', signal_type='range_angle', color_scale=False, 
+                                rotation=False, save_img=False, plot_img=True)
+
+    for annotation_type in ['sparse', 'dense', 'box']:
+        for i, instance in enumerate(instances):
+            points = annotations[instance]['range_angle'][annotation_type]
+            annot_generator = AnnotationGenerator(ra_matrix.shape, points)
+            if annotation_type is 'sparse':
+                annots = annot_generator.get_points()
+            elif annotation_type is 'dense':
+                annots = annot_generator.get_mask()
+            else:
+                annots = annot_generator.get_box()
+            signal_visualizer.add_annotation(i, annots, annotation_type)
+        print('Range-Angle with {} annotations:'.format(annotation_type))
+        signal_visualizer.save_multiple_annotations(path=f'/root/workspace/temp/ra_{annotation_type}.png',
+                                                    signal_type='range_angle', color_scale=False,
+                                                    rotation=False, save_img=False, plot_img=True)
+        signal_visualizer.reset_annotation()
+
+
+if __name__ == '__main__':
+    main()
+
