10d9203d0bdbeefb30cb8294dac40070d93c472a
Paul <95068443+paulchen2713@users.noreply.github.com>
2022-10-25 21:23:35 +0800

    Add files via upload

---
+++ reformatted.py
@@ -0,0 +1,40 @@
+import json
+
+CURR_PATH = "D:/Datasets/CARRADA/2020-02-28-13-09-58/annotations/box/"
+# "range_doppler_light.json", "range_angle_light.json"
+with open(CURR_PATH + "range_doppler_light.json", "r") as json_file:
+    """
+    # there are two ways to read json file
+    # data = json.load(json_file)         # one using json.load(), which load the json_file
+    # data = json.loads(json_file.read()) # make sure you add ".read()" when using json.loads(), the "s" means string
+    """
+    data = json.loads(json_file.read())
+    # print(type(data)) # <class 'dict'>
+
+# extract all keys from the dict, and store them in a list()
+all_keys = list(data.keys())
+print(data[f"{all_keys[28]}"]["boxes"])
+# print(data[f"{all_keys[0]}"]["boxes"])  # [[69, 32, 72, 35]] <class 'list'>
+# print(data[f"{all_keys[0]}"]["labels"]) # [1] <class 'list'>
+
+for key in all_keys:
+    print(key)
+    # with open(CURR_PATH + f"labels/{key}.txt", "a") as label_txt_file:
+    #     one_line = str(data[key]['labels'][0]) 
+    #     label_txt_file.write(one_line)
+
+
+
+
+
+
+
+# store an indentted version of the annotations as .txt file
+def store():
+    json_string = json.dumps(data, indent=4)
+    # print(type(json_string)) # <class 'str'>
+    # print(json_string)
+    with open(CURR_PATH + "rd_formatted.txt", "w") as text_file:
+        # write the whole string into a .txt file, it'll also store the indentation
+        text_file.write(json_string)
+
