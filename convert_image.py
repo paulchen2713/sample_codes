01fcb3f6445833b32a5c2da0cfa9cecf15e4c2ea
Paul <95068443+paulchen2713@users.noreply.github.com>
2022-11-01 09:00:34 +0800

    Update convert_image.py

--- convert_image.py
+++ convert_image.py
@@ -38,53 +38,59 @@ def read_txt_file(file_name=""):
     return dir_names
 # temp = read_txt_file("validated_seqs.txt")
 
-tic = time.perf_counter()
-for dir_name in dir_names: # [23:24]:
-    # e.g. "D:/Datasets/CARRADA/2020-02-28-13-09-58/annotations/box/"
-    print(f"current directory: {dir_name}")
 
-    # set the file path
-    seq_path = DATASET + dir_name + '/'
-    print(f"current seq path: {seq_path}")
+def main():
+    for dir_name in dir_names: # [23:24]:
+        # e.g. "D:/Datasets/CARRADA/2020-02-28-13-09-58/annotations/box/"
+        print(f"current directory: {dir_name}")
 
-    # "range_doppler_light.json", "range_angle_light.json"
-    with open(DATASET + f"{dir_name}/annotations/box/" + "range_doppler_light.json", "r") as json_file:
-        data = json.loads(json_file.read())
-    # extract all keys from the dict, and store them in a list()
-    all_keys = list(data.keys())
+        # set the file path
+        seq_path = DATASET + dir_name + '/'
+        print(f"current seq path: {seq_path}")
 
-    for key in all_keys: # [62:63]:
-        print(f"frame name: \"{key}\"")
+        # "range_doppler_light.json", "range_angle_light.json"
+        with open(DATASET + f"{dir_name}/annotations/box/" + "range_doppler_light.json", "r") as json_file:
+            data = json.loads(json_file.read())
+        # extract all keys from the dict, and store them in a list()
+        all_keys = list(data.keys())
 
-        # set matrix and image path
-        rd_path = seq_path + 'range_doppler_numpy/' + key + '.npy'
-        # ra_path = seq_path + 'range_angle_numpy/' + key + '.npy'
-        # img_path = seq_path + 'camera_images/' + key + '.jpg'
+        for key in all_keys: # [62:63]:
+            print(f"frame name: \"{key}\"")
 
-        # load the RDM, RAM
-        rd_matrix = np.load(rd_path)
-        # ra_matrix = np.load(ra_path)
-        # print(f"rd_matrix.shape = {rd_matrix.shape}") # (256, 64)
-        # print(f"ra_matrix.shape = {ra_matrix.shape}") # (256, 256)
+            # set matrix and image path
+            rd_path = seq_path + 'range_doppler_numpy/' + key + '.npy'
+            # ra_path = seq_path + 'range_angle_numpy/' + key + '.npy'
+            # img_path = seq_path + 'camera_images/' + key + '.jpg'
 
-        plt.matshow(rd_matrix, interpolation="nearest")
-        plt.plasma()
-        plt.axis('off')
-        store_path = seq_path + "RD_maps/images/"
-        # print(f"store path: \"{store_path}\"") # e.g. "D:/Datasets/CARRADA/2020-02-28-13-09-58/RD_maps/images/""
-        plt.savefig(store_path + f'{key}.png', bbox_inches='tight', pad_inches=0)
+            # load the RDM, RAM
+            rd_matrix = np.load(rd_path)
+            # ra_matrix = np.load(ra_path)
+            # print(f"rd_matrix.shape = {rd_matrix.shape}") # (256, 64)
+            # print(f"ra_matrix.shape = {ra_matrix.shape}") # (256, 256)
 
-        # RuntimeWarning: More than 20 figures have been opened. Figures created through the pyplot interface 
-        # (`matplotlib.pyplot.figure`) are retained until explicitly closed and may consume too much memory. 
-        # (To control this warning, see the rcParam `figure.max_open_warning`).
-        
-        plt.clf() # clears the entire current figure 
-        plt.close(plt.gcf()) # ref. https://heitorpb.github.io/bla/2020/03/18/close-matplotlib-figures/
+            plt.matshow(rd_matrix, interpolation="nearest")
+            plt.plasma()
+            plt.axis('off')
+            store_path = seq_path + "RD_maps/images/"
+            # print(f"store path: \"{store_path}\"") # e.g. "D:/Datasets/CARRADA/2020-02-28-13-09-58/RD_maps/images/""
+            plt.savefig(store_path + f'{key}.png', bbox_inches='tight', pad_inches=0)
 
-        # plt.show()
+            # RuntimeWarning: More than 20 figures have been opened. Figures created through the pyplot interface 
+            # (`matplotlib.pyplot.figure`) are retained until explicitly closed and may consume too much memory. 
+            # (To control this warning, see the rcParam `figure.max_open_warning`).
+            
+            plt.clf() # clears the entire current figure 
+            plt.close(plt.gcf()) # ref. https://heitorpb.github.io/bla/2020/03/18/close-matplotlib-figures/
 
-toc = time.perf_counter()
-duration = toc - tic
-print(f"duration: {duration:0.4f} seconds") # duration: 1233.5710 seconds
+            # plt.show()
 
+
+
+if __name__ == '__main__':
+    tic = time.perf_counter()
+    main()
+    toc = time.perf_counter()
+    duration = toc - tic
+    print(f"duration: {duration:0.4f} seconds") # duration: 1233.5710 seconds
+    print(duration)
 
