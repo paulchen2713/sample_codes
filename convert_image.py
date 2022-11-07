7ce68b445e44ae93f771d9bd11ef075b35a31ab0
Paul <95068443+paulchen2713@users.noreply.github.com>
2022-11-07 10:58:00 +0800

    Add files via upload

--- convert_image.py
+++ convert_image.py
@@ -16,6 +16,7 @@ import time
 
 # set the dataset path
 DATASET = 'D:/Datasets/CARRADA/'
+DATASET2 = 'D:/Datasets/CARRADA2/'
 
 # directory names, number of directorie: 30
 dir_names = ['2019-09-16-12-52-12', '2019-09-16-12-55-51', '2019-09-16-12-58-42', '2019-09-16-13-03-38', '2019-09-16-13-06-41', 
@@ -40,7 +41,7 @@ def read_txt_file(file_name=""):
 
 
 def main():
-    for dir_name in dir_names: # [23:24]:
+    for dir_name in dir_names: # [23:24]: # 
         # e.g. "D:/Datasets/CARRADA/2020-02-28-13-09-58/annotations/box/"
         print(f"current directory: {dir_name}")
 
@@ -49,30 +50,33 @@ def main():
         print(f"current seq path: {seq_path}")
 
         # "range_doppler_light.json", "range_angle_light.json"
-        with open(DATASET + f"{dir_name}/annotations/box/" + "range_doppler_light.json", "r") as json_file:
+        file_index = ["range_doppler_light.json", "range_angle_light.json"]
+        with open(DATASET + f"{dir_name}/annotations/box/" + f"{file_index[1]}", "r") as json_file:
             data = json.loads(json_file.read())
         # extract all keys from the dict, and store them in a list()
         all_keys = list(data.keys())
 
-        for key in all_keys: # [62:63]:
+        for key in all_keys: # [62:63]: # 
             print(f"frame name: \"{key}\"")
 
             # set matrix and image path
             rd_path = seq_path + 'range_doppler_numpy/' + key + '.npy'
-            # ra_path = seq_path + 'range_angle_numpy/' + key + '.npy'
+            ra_path = seq_path + 'range_angle_numpy/' + key + '.npy'
             # img_path = seq_path + 'camera_images/' + key + '.jpg'
 
             # load the RDM, RAM
             rd_matrix = np.load(rd_path)
-            # ra_matrix = np.load(ra_path)
+            ra_matrix = np.load(ra_path)
             # print(f"rd_matrix.shape = {rd_matrix.shape}") # (256, 64)
             # print(f"ra_matrix.shape = {ra_matrix.shape}") # (256, 256)
 
-            plt.matshow(rd_matrix, interpolation="nearest")
+            seq_path2 = DATASET2 + "RA/" + dir_name + '/'
+            store_path = seq_path2 + "images/"
+            # print(f"store path: \"{store_path}\"") # e.g. "D:/Datasets/CARRADA/2020-02-28-13-09-58/RD_maps/images/""
+            
+            plt.matshow(ra_matrix, interpolation="nearest")
             plt.plasma()
             plt.axis('off')
-            store_path = seq_path + "RD_maps/images/"
-            # print(f"store path: \"{store_path}\"") # e.g. "D:/Datasets/CARRADA/2020-02-28-13-09-58/RD_maps/images/""
             plt.savefig(store_path + f'{key}.png', bbox_inches='tight', pad_inches=0)
 
             # RuntimeWarning: More than 20 figures have been opened. Figures created through the pyplot interface 
@@ -88,9 +92,11 @@ def main():
 
 if __name__ == '__main__':
     tic = time.perf_counter()
-    main()
+    # main()
     toc = time.perf_counter()
     duration = toc - tic
-    print(f"duration: {duration:0.4f} seconds") # duration: 1233.5710 seconds
+    print(f"duration: {duration:0.4f} seconds") 
     print(duration)
+    # rd_matrix duration: 1233.5710 seconds
+    # ra_matrix duration: 871.3378964 seconds
 
--- convert_label.py
+++ convert_label.py
@@ -10,9 +10,11 @@ Created on Tue Oct 30 17:17:12 2022
 """
 
 import json
+import time
 
 # set the dataset path
 DATASET = 'D:/Datasets/CARRADA/'
+DATASET2 = 'D:/Datasets/CARRADA2/'
 
 # directory names, number of directorie: 30
 dir_names = ['2019-09-16-12-52-12', '2019-09-16-12-55-51', '2019-09-16-12-58-42', '2019-09-16-13-03-38', '2019-09-16-13-06-41', 
@@ -36,14 +38,15 @@ def read_txt_file(file_name=""):
 
 def main():
     dir_names = read_txt_file("validated_seqs.txt")
-    
-    for dir_name in dir_names:
+
+    for dir_name in dir_names: # [23:24]: # 
         # e.g. "D:/Datasets/CARRADA/2019-09-16-12-58-42/annotations/box/"
         print(f"current directory: {dir_name}")
 
-        CURR_PATH = f"D:/Datasets/CARRADA/{dir_name}/annotations/box/"
         # "range_doppler_light.json", "range_angle_light.json"
-        with open(CURR_PATH + "range_doppler_light.json", "r") as json_file:
+        file_index = ["range_doppler_light.json", "range_angle_light.json"]
+        with open(DATASET + f"{dir_name}/annotations/box/" + f"{file_index[1]}", "r") as json_file:
+        # with open(CURR_PATH + "range_doppler_light.json", "r") as json_file:
             """
             # there are two ways to read json file
             # data = json.load(json_file)         # one using json.load(), which load the json_file
@@ -57,34 +60,61 @@ def main():
         # print(data[f"{all_keys[0]}"]["boxes"])  # [[69, 32, 72, 35]] <class 'list'>
         # print(data[f"{all_keys[0]}"]["labels"]) # [1] <class 'list'>
 
-        for key in all_keys:
+        for key in all_keys: # [62:63]: # 
             print(f"frame name: \"{key}\"")
-            # print(f"everything: {data[key]}")
+
+            # RD_maps, RA_maps
+            RDM_PATH = f"D:/Datasets/CARRADA2/RD2/{dir_name}/labels/" # path that we store our labels
+            RAM_PATH = f"D:/Datasets/CARRADA2/RA/{dir_name}/labels/" # path that we store our labels
+
+            # with open(RDM_PATH + f"log_RA.txt", "a") as label_txt_file:
+            with open(RAM_PATH + f"0000_log_{dir_name}.txt", "a") as label_txt_file:
+                print(f"{data[key]}", file=label_txt_file)
             # print(f"num of boxes: {len(data[key]['boxes'])}")
             # print(f"num of labels: {len(data[key]['labels'])}")
 
             if (len(data[key]['boxes']) != len(data[key]['labels'])): 
                 print("boxes and labels are mismatched!")
 
-            with open(CURR_PATH + f"labels/{key}.txt", "w") as label_txt_file:
+            # with open(RDM_PATH + f"{key}.txt", "w") as label_txt_file:
+            with open(RAM_PATH + f"{key}.txt", "w") as label_txt_file:
                 # in each rd_matrix / image it may contain 1~3 possible targets
                 for index in range(0, len(data[key]['boxes'])):
                     # print(data[key]['boxes'][index])
                     # print(data[key]['labels'][index])
 
-                    x, y, w, h = data[key]['boxes'][index][0:4]   # extract COCO format in absolute scale
-                    x, y, w, h = x / 256, y / 64, w / 256, h / 64 # convert to YOLO format in relative scale
-                    # print(f"x, y, w, h = {x}, {y}, {w}, {h}")
                     class_index = data[key]['labels'][index] - 1
                     # print(f"class_index = {class_index}")
 
-                    print(f"{class_index} {x} {y} {w} {h}", file=label_txt_file) # redirect 'print()' output to a file
+                    # [x, y, width, height] is COCO format in absolute scale
+                    # [x_min, y_min, x_max, y_max] is Pascal_VOC format in absolute scale
+                    x_min, y_min, x_max, y_max = data[key]['boxes'][index][0:4]   # extract Pascal_VOC / COCO format in absolute scale
+                    print(f"{class_index} {x_min} {y_min} {x_max} {y_max}")
 
+                    x, y = (x_max + x_min) / 2, (y_max + y_min) / 2
+                    w, h = (y_max - y_min), (x_max - x_min)
+                    
+                    x, y, w, h = x / 256, y / 256, w / 256, h / 256 # RA map
+                    # x, y, w, h = x / 256, y / 64, w / 256, h / 64 # RD map, convert from COCO format to YOLO format in relative scale
+
+                    # print(f"x, y, w, h = {x}, {y}, {w}, {h}")
+                    
+                    print(f"{class_index} {x} {y} {w} {h}", file=label_txt_file) # redirect 'print()' output to a file
+                    
                     # print("---------------------------")
 
 
 if __name__ == '__main__':
+    tic = time.perf_counter()
     # main()
-    dir_names = read_txt_file("validated_seqs.txt")
-    print(dir_names)
+    toc = time.perf_counter()
+    duration = toc - tic
+    print(f"duration: {duration:0.4f} seconds") 
+    print(duration)
+    # rd_matrix duration: _ seconds
+    # ra_matrix duration: 4.3379489 seconds
+    
+
+
+
 
